ec73780eefb7ab10a626dbba2b4b7694d8892ca5
Qbik222 <97127218+Qbik222@users.noreply.github.com>
2022-10-13 01:30:56 +0300

    add elem teacher in block about on a main page

--- src/js/modules/hideLogo.js
+++ src/js/modules/hideLogo.js
@@ -1,18 +1,15 @@
 function hideLogo(content, slider){
     const logo = document.querySelector(".logo")
-    if (document.title !== "Rhythm" ){
+    if (window.innerWidth < 1030 && window.innerWidth > 868){
         slider = content;
     }
-
+    if (document.title !== "Rhythm"){
+        slider = content;
+    }
 
     function hideLogoByScroll(){
         let sliderPosition = slider.getBoundingClientRect().y;
         let logoHeight = logo.getBoundingClientRect().height;
-
-
-
-
-
 
         if (sliderPosition <= logoHeight ){
             logo.classList.add("logoHide")  
